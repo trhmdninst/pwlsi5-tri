487d418d69bc62717fab36cf4b3b7e199d33aef8
trhmdninst <trirahmadaninst@gmail.com>
2024-12-01 00:35:47 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('mahasiswa', function () {
+    return view('mahasiswa');
+});
+
+Route::get('profile', function () {
+    return view('profile');
+});
